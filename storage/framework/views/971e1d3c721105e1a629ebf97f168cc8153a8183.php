1de10ff345c0d46edfc65a1da5e3513f24182ae8
OutOfTime <126634930+OutOfTime080102@users.noreply.github.com>
2023-07-31 15:43:05 +0700

    Add files via upload

---
+++ storage/framework/views/971e1d3c721105e1a629ebf97f168cc8153a8183.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Đăng nhập</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Quicksand"
+      rel="stylesheet"
+    />
+    <link
+      href="http://127.0.0.1:5500/resources/css/login.css"
+      rel = "stylesheet"
+    />
+  </head>
+  <body>
+    <div
+      class="
+        fullview
+      "
+    >
+      <img
+        class="picture"
+        src="http://127.0.0.1:5500/resources/images/picture.png"
+      />
+      <div
+        class="frontpicture"
+      ></div>
+      <div
+        class="backgroundlogin"
+      ></div>
+      <div class="login">
+        <div
+          class="svgback"
+        >
+          <a href="#" ><img
+            style="width: 25px; height: 25px"
+            src="http://127.0.0.1:5500/resources/images/iconback.svg"
+          /></a>
+        </div>
+        <div
+          class="logintext"
+        >
+        <a class="one" href="#">ĐĂNG&nbsp;NHẬP</a>
+        </div>
+      </div>
+      <img
+        class="eistudyiconwhite"
+        "
+        src="http://127.0.0.1:5500/resources/images/EIStudy%201.png"
+      />
+      <img
+        class="eistudyiconpink"
+        src="http://127.0.0.1:5500/resources/images/z3007233494654_15ca45a569fa4c55c533e9fa0bdcc435%202.png"
+      />
+      <div
+        class="welcome"
+      >
+        <b>Chào mừng bạn đã đến với EI Germany!</b>
+      </div>
+      <div
+        class="email"
+      >
+        <input class="boxemail" type="email" placeholder="Email">
+          <div
+          class="boxemailicon"
+        >
+        <img
+            class="emailicon"
+            src="http://127.0.0.1:5500/resources/images/iconemail.svg"
+          />
+        </div>
+        
+      </div>
+      <div
+        class="password"
+      >
+       <input
+          class="boxpassword"
+          type = "password"
+          placeholder="Mật khẩu"
+        >
+        <div
+        class="boxpassicon"
+        >
+          <img
+            class="iconpass"
+            src="http://127.0.0.1:5500/resources/images/iconpassword.svg"
+          />
+        </div>
+      </div>
+      <div
+        class="boxlogin"
+      >
+        <button
+          type="submit"
+          class="button"
+        >
+        <div
+          class="boxlogintext"
+        >
+          Đăng nhập
+        </div>
+        </button>
+      </div>
+      
+      <div
+        class="forgetpass"
+      >
+      <a class="two" href="#">Quên mật khẩu?</a>
+      </div>
+    </div>
+    <script src="http://127.0.0.1:5500/resources/js/password.js"></script>
+  </body>
+</html>
+<?php /**PATH D:\laravel-app\resources\views/login.blade.php ENDPATH**/ ?>
